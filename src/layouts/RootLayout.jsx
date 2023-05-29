db632b02fdb10955b724676e9c72d5172a624d7f
Manir Uddin <manir22sust@gmail.com>
2023-05-29 18:18:11 +0200

    Router Provider, createBrowserRouter & Outlet

---
+++ src/layouts/RootLayout.jsx
@@ -0,0 +1,18 @@
+import { NavLink, Outlet } from "react-router-dom";
+
+export const RootLayout = () => {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1> Job a Router</h1>
+          <NavLink to="/"> Home</NavLink>
+          <NavLink to="about"> About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
